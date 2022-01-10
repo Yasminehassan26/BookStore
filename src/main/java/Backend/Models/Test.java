db6eb4965cc054f8e2fda50bdaec30de642dd6c7
ahmedhazem <amhmamgs.2000@gmail.com>
2022-01-10 04:19:26 +0200

    signIn&signUp

--- src/main/java/Backend/Models/Test.java
+++ src/main/java/Backend/Models/Test.java
@@ -5,43 +5,48 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Test {
-    public static void main(String[] args) throws SQLException {
-        DatabaseManager db = DatabaseManager.getInstance("root", "26101999");
-        Connection c = db.getConnection();
-        ResultSet r = db.executeQuery("SELECT * FROM BOOK;");
+	public static void main(String[] args) throws SQLException {
+		// DatabaseManager db = DatabaseManager.getInstance("root", "26101999");
+		// Connection c = db.getConnection();
+		// ResultSet r = db.executeQuery("SELECT * FROM BOOK;");
 //        while (r.next())
 //        {
 //            System.out.println(r.getInt("ISBN"));
 //            System.out.println(r.getString("title"));
 //        }
 
-//        User user = new User();
-//        UserBasicInfo userr = new UserBasicInfo();
-//        userr.setEmail("a@gmail.com");
-//        userr.setFirstName("Ahmed");
-//        userr.setLastName("Hazemmmm");
-//        userr.setPhoneNumber("0293873");
-//        userr.setShippingAddress("kfkfkeoe");
-//        userr.setPrivilege(2);
-//        userr.setId(1);
-//        user.editPersonalInfo(userr);
-//
-        Book book = new Book();
-        Publisher p = new Publisher();
-        p.setName("ahmed");
-        book.setISBN(3);
-        book.setPrice(56.94);
-        book.setCategory(Category.Art);
-        book.setTitle("hdhks");
-        book.setThreshold(5);
-        book.setNumberOfCopies(10);
-        book.setPublicationDate("2023");
-        book.setPublisher(p);
+        //User user = new User();
+        UserBasicInfo userr = new UserBasicInfo();
+        userr.setUsername("testsignup1");
+        userr.setPassword("testsignup1");
+        userr.setEmail("testsignup1@gmail.com");
+        userr.setFirstName("testsignup1");
+        userr.setLastName("testsignup1");
+        userr.setPhoneNumber("testsignup1");
+        userr.setShippingAddress("testsignup1");
+        userr.setPrivilege(0);
+        //userr.setId(1);
+       //user.editPersonalInfo(userr);
 
-        Manager manager = new Manager();
-        manager.addNewBook(book);
+		Book book = new Book();
+		Publisher p = new Publisher();
+		p.setName("ahmed");
+		book.setISBN(3);
+		book.setPrice(56.94);
+		book.setCategory(Category.Art);
+		book.setTitle("hdhks");
+		book.setThreshold(5);
+		book.setNumberOfCopies(10);
+		book.setPublicationDate("2023");
+		book.setPublisher(p);
+
+		// Manager manager = new Manager();
+		// manager.addNewBook(book);
 //        manager.modifyBook(book);
-
-
-    }
+		
+		
+		User u = new User();
+		//u.signIn("hazem", "1111112");
+		System.out.println(u.signUp(userr));
+	}
 }
--- src/main/java/Backend/Models/User.java
+++ src/main/java/Backend/Models/User.java
@@ -1,6 +1,5 @@
 package Backend.Models;
 
-
 import lombok.Data;
 
 import java.sql.ResultSet;
@@ -11,100 +10,136 @@ import java.util.List;
 @Data
 public class User {
 
-    private int id;
-    private String username;
-    private String password;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phoneNumber;
-    private String shippingAddress;
-    private Cart shoppingCart;
+	private int id;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private String shippingAddress;
+	private Cart shoppingCart;
 
-    public void editPersonalInfo(UserBasicInfo user) throws SQLException {
-        BookStore.databaseManager.executeQuery("UPDATE USER SET UserName = '" + user.getUsername()
-                + "',Password = '" + user.getPassword() + "',First_Name = '" + user.getFirstName() + "',Last_Name = '" + user.getLastName() + "',Email = '" + user.getEmail() +
-                "',Phone_Number = '" + user.getPhoneNumber() + "',Shipping_Address = '" + user.getShippingAddress() + "',privilege = " + user.getPrivilege() +
-                " WHERE User_id = " + user.getId());
-    }
+	public void editPersonalInfo(UserBasicInfo user) throws SQLException {
+		BookStore.databaseManager.executeQuery("UPDATE USER SET UserName = '" + user.getUsername() + "',Password = '"
+				+ user.getPassword() + "',First_Name = '" + user.getFirstName() + "',Last_Name = '" + user.getLastName()
+				+ "',Email = '" + user.getEmail() + "',Phone_Number = '" + user.getPhoneNumber()
+				+ "',Shipping_Address = '" + user.getShippingAddress() + "',privilege = " + user.getPrivilege()
+				+ " WHERE User_id = " + user.getId());
+	}
 
-    private Book mapToBook(ResultSet result) throws SQLException {
-        Book add = new Book();
-        int ISBN = result.getInt("ISBN");
-        add.setISBN(ISBN);
-        add.setCategory(Category.valueOf(result.getNString("Category")));
-        add.setNumberOfCopies(result.getInt("Quantity"));
-        add.setPrice(Double.valueOf(result.getInt("Price")));
-        add.setPublicationDate(result.getString("Publication_year"));
-        add.setThreshold(result.getInt("Threashold"));
-        add.setTitle(result.getNString("Title"));
-        List<String> authors = new ArrayList<>();
-        ResultSet Auth = Manager.getAuthors(ISBN);
-        while(Auth.next()){
-            authors.add(Auth.getNString("Author_name"));
-        }
-        add.setAuthors(authors);
-        ResultSet publishers = Manager.getPublisher(result.getNString("Publisher"));
-        publishers.next();
-        Publisher publisher = new Publisher();
-        publisher.setName(result.getString("Publisher"));
-        publisher.setAddress(publishers.getString("Address"));
-        publisher.setTelephoneNumbers(publishers.getString("Number"));
-        add.setPublisher(publisher);
-        return add;
-    }
+	private Book mapToBook(ResultSet result) throws SQLException {
+		Book add = new Book();
+		int ISBN = result.getInt("ISBN");
+		add.setISBN(ISBN);
+		add.setCategory(Category.valueOf(result.getNString("Category")));
+		add.setNumberOfCopies(result.getInt("Quantity"));
+		add.setPrice(Double.valueOf(result.getInt("Price")));
+		add.setPublicationDate(result.getString("Publication_year"));
+		add.setThreshold(result.getInt("Threashold"));
+		add.setTitle(result.getNString("Title"));
+		List<String> authors = new ArrayList<>();
+		ResultSet Auth = Manager.getAuthors(ISBN);
+		while (Auth.next()) {
+			authors.add(Auth.getNString("Author_name"));
+		}
+		add.setAuthors(authors);
+		ResultSet publishers = Manager.getPublisher(result.getNString("Publisher"));
+		publishers.next();
+		Publisher publisher = new Publisher();
+		publisher.setName(result.getString("Publisher"));
+		publisher.setAddress(publishers.getString("Address"));
+		publisher.setTelephoneNumbers(publishers.getString("Number"));
+		add.setPublisher(publisher);
+		return add;
+	}
 
-    public List<Book> searchByISBN(int ISBN) throws SQLException {
-        List<Book> books = new ArrayList<>();
-        ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_ISBN("+ISBN+")");
-        while(result.next()){
-            books.add(mapToBook(result));
-        }
-        return books;
-    }
+	public List<Book> searchByISBN(int ISBN) throws SQLException {
+		List<Book> books = new ArrayList<>();
+		ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_ISBN(" + ISBN + ")");
+		while (result.next()) {
+			books.add(mapToBook(result));
+		}
+		return books;
+	}
 
-    public List<Book> searchByTitle(String Title) throws SQLException {
-        List<Book> books = new ArrayList<>();
-        ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_Title('"+Title+"')");
-        while(result.next()){
-            books.add(mapToBook(result));
-        }
-        return books;
-    }
+	public List<Book> searchByTitle(String Title) throws SQLException {
+		List<Book> books = new ArrayList<>();
+		ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_Title('" + Title + "')");
+		while (result.next()) {
+			books.add(mapToBook(result));
+		}
+		return books;
+	}
 
-    public List<Book> searchByPublisher(String publisher) throws SQLException {
-        List<Book> books = new ArrayList<>();
-        ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_Publisher('"+publisher+"')");
-        while(result.next()){
-            books.add(mapToBook(result));
-        }
-        return books;
-    }
+	public List<Book> searchByPublisher(String publisher) throws SQLException {
+		List<Book> books = new ArrayList<>();
+		ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_Publisher('" + publisher + "')");
+		while (result.next()) {
+			books.add(mapToBook(result));
+		}
+		return books;
+	}
 
-    public List<Book> searchByCategory(String Category) throws SQLException {
-        List<Book> books = new ArrayList<>();
-        ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_Category('"+Category+"')");
-        while(result.next()){
-            books.add(mapToBook(result));
-        }
-        return books;
-    }
+	public List<Book> searchByCategory(String Category) throws SQLException {
+		List<Book> books = new ArrayList<>();
+		ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_Category('" + Category + "')");
+		while (result.next()) {
+			books.add(mapToBook(result));
+		}
+		return books;
+	}
 
-    public List<Book> searchByAuthor(String author) throws SQLException {
-        List<Book> books = new ArrayList<>();
-        ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_Author('"+author+"')");
-        while(result.next()){
-            books.add(mapToBook(result));
-        }
-        return books;
-    }
+	public List<Book> searchByAuthor(String author) throws SQLException {
+		List<Book> books = new ArrayList<>();
+		ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_Author('" + author + "')");
+		while (result.next()) {
+			books.add(mapToBook(result));
+		}
+		return books;
+	}
 
-    public List<Book> searchByYear(String year) throws SQLException {
-        List<Book> books = new ArrayList<>();
-        ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_year('"+year+"')");
-        while(result.next()){
-            books.add(mapToBook(result));
-        }
-        return books;
-    }
+	public List<Book> searchByYear(String year) throws SQLException {
+		List<Book> books = new ArrayList<>();
+		ResultSet result = BookStore.databaseManager.executeQuery("CALL search_By_year('" + year + "')");
+		while (result.next()) {
+			books.add(mapToBook(result));
+		}
+		return books;
+	}
+
+	public boolean signIn(String username, String password) throws SQLException {
+		ResultSet result = BookStore.databaseManager
+				.executeQuery("select username,password from user where username='" + username + "'");
+		result.next();
+		try {
+			if (result.getString("password").equals(password))
+				return true;
+			else
+				return false;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+
+	public int signUp(UserBasicInfo new_user) throws SQLException {
+		ResultSet result = BookStore.databaseManager
+				.executeQuery("select username from user where username='" + new_user.getUsername() + "'");
+		if (result.next())
+			return -1; // username is taken
+		ResultSet result1 = BookStore.databaseManager
+				.executeQuery("select username from user where email='" + new_user.getEmail() + "'");
+		if (result1.next())
+			return -2; // email is taken
+		try {
+			BookStore.databaseManager.executeQuery("insert into USER values (default,'" + new_user.getUsername() + "','"
+					+ new_user.getPassword() + "','" + new_user.getFirstName() + "','" + new_user.getLastName() + "','"
+					+ new_user.getEmail() + "','" + new_user.getPhoneNumber() + "','" + new_user.getShippingAddress()
+					+ "',0)");
+			return 0;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			return -3; // error in query ex: data input is invalid
+		}
+	}
 }
